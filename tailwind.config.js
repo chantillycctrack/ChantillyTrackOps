06e2bfdd683d3ff1a86ceadc860a165a10d25588
chantillycctrack <chantillycctrack@gmail.com>
2026-04-19 22:37:00 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class', // This allows us to toggle 'dark' mode manually
+  theme: {
+    extend: {
+      colors: {
+        chantilly: {
+          light: '#7B5EB3',
+          DEFAULT: '#4B2E83', // Your official purple
+          dark: '#2D1B50',
+        },
+        darkBg: '#0F172A', // Deep navy/charcoal for dark mode
+      },
+    },
+  },
+  plugins: [],
+}
