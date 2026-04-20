8f9c41b5c5c0c8fb7b6a7291e37416c96e5f8c4d
chantillycctrack <chantillycctrack@gmail.com>
2026-04-19 22:29:20 -0400

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,26 +1,20 @@
-import { auth, googleProvider } from "../firebaseConfig";
-import { signInWithPopup, signOut } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const Login = ({ user }) => {
-  const loginWithGoogle = () => {
-    signInWithPopup(auth, googleProvider)
-      .catch((error) => console.error("Login failed:", error));
-  };
-
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      {!user ? (
-        <button onClick={loginWithGoogle} className="purple-btn">
-          Coach Login (Google)
-        </button>
-      ) : (
-        <div>
-          <p>Logged in as: {user.email}</p>
-          <button onClick={() => signOut(auth)}>Logout</button>
-        </div>
-      )}
-    </div>
-  );
+// PASTE YOUR KEYS FROM FIREBASE BELOW THIS LINE
+const firebaseConfig = {
+  apiKey: "AIzaSyB3qNOOkt41kVycFJJdkqMtRy_RL_uQgwA",
+  authDomain: "chantillytrackops.firebaseapp.com",
+  projectId: "chantillytrackops",
+  storageBucket: "chantillytrackops.firebasestorage.app",
+  messagingSenderId: "808372506425",
+  appId: "1:808372506425:web:9a0a57c5382c864577a2e8",
+  measurementId: "G-YYL1RYS0M1"
 };
+// PASTE YOUR KEYS ABOVE THIS LINE
 
-export default Login;
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
